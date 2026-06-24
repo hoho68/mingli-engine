a67e58a34bab5dfcd10cda4eb150707b70c2b73e
iiilxs <iiilxs@qq.com>
2026-06-24 11:29:59 +0800

    chore: clean up temp files after test fix session

--- check_indent.py
+++
@@ -1,5 +0,0 @@
-with open("tests/unit/test_materials_audit.py", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-for i in range(1319, 1342):
-    if i < len(lines):
-        print(str(i+1) + ":" + repr(lines[i]))
